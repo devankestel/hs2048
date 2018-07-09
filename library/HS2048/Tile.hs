af5fe3ce43a719bd5ec6a3e2d94badf9f49f6ea3
Devan Kestel <devan.kestel@gmail.com>
2018-07-09 02:20:12 -0400

    Add basic tile functions.

--- library/HS2048/Tile.hs
+++ library/HS2048/Tile.hs
@@ -1,1 +1,20 @@
-module HS2048.Tile () where
+module HS2048.Tile 
+    ( Tile
+    , parse
+    , render
+    , score
+    ) where
+
+type Tile = Maybe Int
+
+parse :: String -> Tile
+parse "-" = Nothing
+parse s = Just (read s)
+
+render :: Tile -> String
+render Nothing = "-"
+render (Just n) = show n
+
+score :: Tile -> Int
+score Nothing = 0
+score (Just n) = n
