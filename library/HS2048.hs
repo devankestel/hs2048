2a2d83f77903ada96b341179d77f0ee2958af7de
Devan Kestel <devan.kestel@gmail.com>
2018-07-09 02:14:51 -0400

    Add tile library file and import to main library file.

--- library/HS2048.hs
+++ library/HS2048.hs
@@ -1,6 +1,8 @@
 -- | An example module.
 module HS2048 (main) where
 
+import HS2048.Tile as HS2048
+
 -- | An example function.
 main :: IO ()
 main = return ()
---
+++ library/HS2048/Tile.hs
@@ -0,0 +1,1 @@
+module HS2048.Tile () where
